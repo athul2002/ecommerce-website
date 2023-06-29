8807708b0b127ff19edfea103f6c7ad602419487
Athul <athulmaliyakkal@gmail.com>
2023-06-30 00:08:03 +0530

    Backend Error Handling
    Handled errors like cast error, unhandled rejection, uncaught exception and made

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,25 @@ const dotenv=require('dotenv');
 const connectDatabase=require('./config/database')
 dotenv.config({path:"backend/config/config.env"})
 
+process.on("uncaughtException",err=>{
+    console.log(`Error:${err.message}`);
+    console.log("Shutting down the server due to unhandled Promise rejection");
+
+    server.close(()=>{
+        process.exit(1);
+    })    
+})
+
 //connecting to database
 connectDatabase()
-app.listen(process.env.PORT,()=>{
+const server = app.listen(process.env.PORT,()=>{
     console.log(`Server started in port ${process.env.PORT}`);
+})
+process.on("unhandledRejection",err=>{
+    console.log(`Error:${err.message}`);
+    console.log("Shutting down the server due to unhandled Promise rejection");
+
+    server.close(()=>{
+        process.exit(1);
+    })
 })
