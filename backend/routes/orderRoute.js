a37bc4b705d98c617ecaad3258b20c141720d86a
Athul <athulmaliyakkal@gmail.com>
2023-07-05 00:35:44 +0530

    Order Route

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const router=express.Router();
+const { isAuthenticatedUser, authorizeRoles }=require('../middleware/auth');
+const { newOrder, getSingleOrder, myOrders, getAllOrder, updateOrder, deleteOrder } = require('../controllers/orderController');
+router.route("/order/new").post(isAuthenticatedUser,newOrder);
+router.route("/order/:id").get(isAuthenticatedUser,getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser,myOrders);
+router.route("/admin/orders").get(isAuthenticatedUser,authorizeRoles("admin"),getAllOrder);
+router.route("/admin/order/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateOrder).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteOrder);
+module.exports=router;
