bcf5a80b1fa0aae9d565a4e2985fe2e362dea284
Athul <athulmaliyakkal@gmail.com>
2023-07-04 21:52:10 +0530

    Update
    Get User Detail, Update User Password, Update User detail, Get all users(Admin only), Get a user (Admin only), Update User Role and Delete User(Admin Only) functionalities added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -123,3 +123,111 @@ exports.forgotPassword = asyncError(async (req, res, next) => {
     await user.save();
     sendToken(user,200,res);
   });
+
+  // Get User Detail
+exports.detailUser=asyncError(async(req,res,next)=>{
+  const user=await User.findById(req.user.id);
+  // if(!user)
+  // {
+  //     return next(new ErrorHandler("User not found",404));
+  // }
+  res.status(200).json({success:true,
+      user});
+  })
+
+  // Update User Password
+exports.updatePassword=asyncError(async(req,res,next)=>{
+  let user=await User.findById(req.user.id).select("+password");
+
+    const isPasswordMatched=await user.comparePassword(req.body.oldPassword);
+
+    if(!isPasswordMatched)
+    {
+        return next(new ErrorHandler("Old Password is incorrect",401));
+    }
+
+    if(req.body.newPassword!==req.body.confirmPassword)
+    {
+      return next(new ErrorHandler("Password does not match",400));
+    }
+
+    user.password=req.body.newPassword;
+    await user.save();
+
+    sendToken(user,200,res);
+})
+  // Update User detail
+exports.updateUserDetail=asyncError(async(req,res,next)=>{
+  const newUserData={
+    name:req.body.name,
+    email:req.body.email,
+  };
+
+  const user=await User.findByIdAndUpdate(req.user.id,newUserData,{
+    new:true,
+    runValidators:true,
+    useFindAndModify:false,
+  });
+
+  res.status(200).json({
+    success:true,
+  });
+});
+
+// Get all users (admin)
+exports.getAllUsers=asyncError(async(req,res)=>{
+  const users=await User.find();
+  res.status(200).json({success:true,
+      users,
+    });
+});
+
+// Get a user (admin)
+exports.getUser=asyncError(async(req,res)=>{
+  const user=await User.findById(req.params.id);
+
+  if(!user)
+  {
+    return next(new ErrorHandler(`User does not exist with id : ${req.params.id}`,400))
+  }
+  res.status(200).json({success:true,
+      user,
+    });
+});
+
+  // Update User Role -- Admin
+  exports.updateUserRole=asyncError(async(req,res,next)=>{
+    const newUserData={
+      name:req.body.name,
+      email:req.body.email,
+      role:req.body.role,
+    };
+  
+    const user=await User.findByIdAndUpdate(req.params.id,newUserData,{
+      new:true,
+      runValidators:true,
+      useFindAndModify:false,
+    });
+  
+    res.status(200).json({
+      success:true,
+    });
+  });
+  
+  // Delete User  -- Admin
+  exports.deleteUser=asyncError(async(req,res,next)=>{
+    const user=await User.findByIdAndUpdate(req.params.id);
+
+    if(!user)
+    {
+      return next(new ErrorHandler(`User does not exist with id: ${req.params.id}`,400))
+    }
+
+    await user.deleteOne();
+    res.status(200).json({
+      success:true,
+      message:"User deleted successfully"
+    });
+  });
+  
+ 
