4f32e96db961e29d4eececf96b3dcb4cb631ab72
Athul <athulmaliyakkal@gmail.com>
2023-07-05 00:35:53 +0530

    Update

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,11 @@ const errorMiddleWare=require('./middleware/error');
 //Route imports
 const product = require('./routes/productRoute');
 const user = require('./routes/userRoute');
+const order=require('./routes/orderRoute');
 
 app.use("/api/v1",product);
 app.use('/api/v1',user);
+app.use('/api/v1',order);
 
 // Middleware for error
 app.use(errorMiddleWare);
