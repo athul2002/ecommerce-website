de7bc9c750029fea9ac818461b1cb3e83928fdf4
Athul <athulmaliyakkal@gmail.com>
2023-07-07 15:09:32 +0530

    Redux Implementation
    With the help of redux, top priority products are displayed in home page and details of specific product when clicked on it

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,39 +1,49 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import {CgMouse} from "react-icons/cg"
 import "./Home.css"
-import Product from './Product.js'
+import Product from './ProductCard'
+import MetaData from '../layout/MetaData'
+import {clearErrors, getProduct} from "../../actions/productAction";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from '../layout/Loader/Loader'
+import { useAlert } from "react-alert"
 
-const product={
-    name:"Blue Tshirt",
-    images:[{url:"https://images.bewakoof.com/t640/men-regular-fit-solid-t-shirt-580000-1685707457-1.jpg"}],
-    price:"Rs 3000",
-    _id:"abhishek",
-}
 const Home = () => {
-  return (
-    <Fragment>
-        <div className="banner">
-            <p>Welcome to Ecommerce</p>
-            <h1>FIND AMAZING PRODUCTS BELOW</h1>
-
-            <a href="#container">
-                <button>
-                    Scroll <CgMouse/>
-                </button>
-            </a>
-        </div>
-        <h2 className="homeHeading">Featured Products</h2>
-        <div className="container" id="container">
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-        </div>
-    </Fragment>
+    const alert=useAlert();
+    const dispatch=useDispatch();
+    const {loading,error,products,productsCount}=useSelector((state)=>state.products)
+    useEffect(() => {
+        if(error)
+        {
+           alert.error(error);
+           dispatch(clearErrors());
+        }
+        dispatch(getProduct());
+    }, [dispatch,error,alert])
+    
+    return (
+        <Fragment>
+            {loading ?<Loader/>:
+                <Fragment >
+                <MetaData title="ECOMMERCE"/>
+                <div className="banner">
+                    <p>Welcome to Ecommerce</p>
+                    <h1>FIND AMAZING PRODUCTS BELOW</h1>
+        
+                    <a href="#container">
+                        <button>
+                            Scroll <CgMouse/>
+                        </button>
+                    </a>
+                </div>
+                <h2 className="homeHeading">Featured Products</h2>
+                <div className="container" id="container">
+                    {products && products.map(product=>(
+                        <Product key={product._id} product={product}/>
+                    ))}
+                </div>
+            </Fragment>}
+        </Fragment>
   )
 }
 
