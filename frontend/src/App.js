fbf02996732a80f502a537249cbaa535c00aeead
Athul <athulmaliyakkal@gmail.com>
2023-07-07 15:08:07 +0530

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,10 @@ import WebFont from "webfontloader"
 import React from "react";
 import Footer from "./component/layout/footer/Footer.js";
 import Home from "./component/Home/Home.js"
+// import Loader from "./component/layout/Loader/Loader.js";
+import ProductDetails from "./component/product/ProductDetails.js";
+// import Loader from "./component/layout/Loader/Loader.js";
+
 function App() {
   React.useEffect(()=>{
     WebFont.load({
@@ -13,11 +17,13 @@ function App() {
       },
     });
   },[]);
+
   return (
     <Router>
       <Header/>
       <Routes>
-      <Route exact path="/" Component={Home}/>
+        <Route exact path="/" Component={Home}/>
+        <Route exact path="/product/:id" Component={ProductDetails}/>
       </Routes>
       <Footer/>
     </Router>
