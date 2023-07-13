594d5eb751019d16e94a7d684002785d301a02cf
Athul <athulmaliyakkal@gmail.com>
2023-07-13 16:13:43 +0530

    Cloudinary
    Added cloudinary feature for uploadinf file

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,15 +4,21 @@ const User=require('../models/userModels');
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
 const crypto=require('crypto');
+const cloudinary=require('cloudinary');
 
 // Register User
 exports.registerUser=asyncError(async(req,res,next)=>{
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
+    folder: "avatars",
+    width: 150,
+    crop: "scale",
+  });
     const {name,email,password}=req.body;
     const user=await User.create({
         name,email,password,
         avatar:{
-            public_id:"This is a sample id",
-            url:"profilePicUrl"
+            public_id:myCloud.public_id,
+            url:myCloud.secure_url
         }
     });
 
