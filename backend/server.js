ced32c1b3ada95ab1fc476a9702eadddb67e9d12
Athul <athulmaliyakkal@gmail.com>
2023-07-13 16:14:09 +0530

    Update
    Updated for enabling cloudinary

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app=require('./app');
 const dotenv=require('dotenv');
-const connectDatabase=require('./config/database')
+const connectDatabase=require('./config/database');
+const cloudinary=require('cloudinary');
 dotenv.config({path:"backend/config/config.env"})
 
 process.on("uncaughtException",err=>{
@@ -13,7 +14,12 @@ process.on("uncaughtException",err=>{
 })
 
 //connecting to database
-connectDatabase()
+connectDatabase();
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+})
 const server = app.listen(process.env.PORT,()=>{
     console.log(`Server started in port ${process.env.PORT}`);
 })
