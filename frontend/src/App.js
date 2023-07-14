559380a380c24e6cc7daa9ce9832da913e7ae52f
Athul <athulmaliyakkal@gmail.com>
2023-07-14 20:05:39 +0530

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,18 @@ import ProductDetails from "./component/product/ProductDetails.js";
 import Products from './component/product/Products.js'
 import Search from './component/product/Search.js'
 import LoginSignUp from "./component/user/LoginSignUp.js";
+import Profile from "./component/user/Profile.js";
 import Store from './Store'
 import { loadUser } from './actions/userActions.js'
+import UserOptions from './component/layout/header/UserOptions.js'
+import { useSelector } from "react-redux";
+import ProtectedRoute from "./component/route/ProtectedRoute.js";
+import UpdateProfile from './component/user/UpdateProfile.js'
+import UpdatePassword from './component/user/UpdatePassword.js'
+import ForgotPassword from './component/user/ForgotPassword.js'
+import ResetPassword from './component/user/ResetPassword.js'
 function App() {
+  const {isAuthenticated, user}=useSelector((state)=>state.user)
   React.useEffect(()=>{
     WebFont.load({
       google:{
@@ -24,6 +33,8 @@ function App() {
   return (
     <Router>
       <Header/>
+      {isAuthenticated && <UserOptions user={user}/>}
+
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route exact path="/product/:id" element={<ProductDetails/>}/>
@@ -31,7 +42,43 @@ function App() {
         <Route exact path="/products/:keyword" element={<Products/>}/>
         <Route path="/search" element={<Search/>}/>
         <Route exact path="/login" element={<LoginSignUp/>}/>
+        <Route
+        exact
+            path="/account"
+            element={
+              <ProtectedRoute isSignedIn={isAuthenticated} user={user}>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        <Route
+        exact
+            path="/me/update"
+            element={
+              <ProtectedRoute isSignedIn={isAuthenticated} user={user}>
+                <UpdateProfile />
+              </ProtectedRoute>
+            }
+          />
+        <Route
+        exact
+            path="/password/update"
+            element={
+              <ProtectedRoute isSignedIn={isAuthenticated} user={user}>
+                <UpdatePassword />
+              </ProtectedRoute>
+            }
+          />
+        <Route
+        exact
+            path="/password/forgot"
+            element={
+              <ForgotPassword />
+            }
+          />
+        <Route exact path="/password/reset/:token" element={<ResetPassword/>} />
       </Routes>
+
       <Footer/>
     </Router>
   );
