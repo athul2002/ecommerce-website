a71cb07eafacfffd56a1f4824ab8659fc188f5e3
Athul <athulmaliyakkal@gmail.com>
2023-07-20 18:26:00 +0530

    Update

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -4,13 +4,15 @@ import { composeWithDevTools } from "redux-devtools-extension"
 import { productDetailReducer, productReducer } from "./reducers/productReducer";
 import { forgotPasswordReducer, profileReducer, userReducer } from "./reducers/userReducer";
 import { cartReducer } from "./reducers/cartReducer";
+import { newOrderReducer } from "./reducers/orderReducer";
 const reducer=combineReducers({
     products:productReducer,
     productDetails:productDetailReducer,
     user:userReducer,
     profile:profileReducer,
     forgotPassword:forgotPasswordReducer,
-    cart:cartReducer
+    cart:cartReducer,
+    newOrder:newOrderReducer
 });
 
 let initialState = {
@@ -18,6 +20,9 @@ let initialState = {
       cartItems: localStorage.getItem("cartItems")
         ? JSON.parse(localStorage.getItem("cartItems"))
         : [],
+      shippingInfo: localStorage.getItem("shippingInfo")
+        ? JSON.parse(localStorage.getItem("shippingInfo"))
+        : {},
     },
   };
 const middleware=[thunk];
