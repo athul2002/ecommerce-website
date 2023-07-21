9834c3f92837c506f3bef08b3885cf84d255665b
Athul <athulmaliyakkal@gmail.com>
2023-07-21 17:16:26 +0530

    New review action

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-import { ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,CLEAR_ERRORS, PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST,PRODUCT_DETAIL_SUCCESS } from "../constants/productConstants";
+import { ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,CLEAR_ERRORS,
+     PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST,PRODUCT_DETAIL_SUCCESS,
+     NEW_REVIEW_REQUEST,
+     NEW_REVIEW_SUCCESS,
+     NEW_REVIEW_FAIL,
+     } from "../constants/productConstants";
 
 export const getProduct=(keyword="",currentPage=1,price=[0,25000 ],category,ratings=0)=>async(dispatch)=>{
     try{
@@ -35,6 +40,25 @@ export const getProductDetails=(id)=>async(dispatch)=>{
     {
         dispatch({
             type:PRODUCT_DETAIL_FAIL,
+            payload:error.response.data.message
+        });
+    }
+};
+export const newReview=(reviewData)=>async(dispatch)=>{
+    try{
+        dispatch({type:NEW_REVIEW_REQUEST});
+        const config={
+           headers: {"Content-Type":"application/json"}
+        }
+        const {data}=await axios.put(`/api/v1/review`,reviewData,config);
+        dispatch({
+            type:NEW_REVIEW_SUCCESS,
+            payload:data,
+        })
+    }catch(error)
+    {
+        dispatch({
+            type:NEW_REVIEW_FAIL,
             payload:error.response.data.message
         });
     }
