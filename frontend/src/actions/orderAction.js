cd6d82ae6d8ca8bc054b64968d086b5c3e98f37d
Athul <athulmaliyakkal@gmail.com>
2023-07-21 17:13:30 +0530

    New order actions
    Added new order actions my orders and get order details which gives all orders and order details

--- frontend/src/actions/orderAction.js
+++ frontend/src/actions/orderAction.js
@@ -1,8 +1,12 @@
 import axios from "axios";
-import { CLEAR_ERRORS, CREATE_ORDER_FAIL, CREATE_ORDER_REQUEST, CREATE_ORDER_SUCCESS } from "../constants/orderConstants"
+import { CLEAR_ERRORS, CREATE_ORDER_FAIL, CREATE_ORDER_REQUEST,
+     CREATE_ORDER_SUCCESS,MY_ORDERS_REQUEST, MY_ORDERS_SUCCESS,MY_ORDERS_FAIL,
+     ORDER_DETAILS_REQUEST,
+     ORDER_DETAILS_SUCCESS,
+     ORDER_DETAILS_FAIL,} from "../constants/orderConstants"
 
 
-export const createOrder=(order)=>async(dispatch,getState)=>{
+export const createOrder=(order)=>async(dispatch)=>{
     try {
         dispatch({
             type:CREATE_ORDER_REQUEST
@@ -17,6 +21,39 @@ export const createOrder=(order)=>async(dispatch,getState)=>{
     } catch (error) {
         dispatch({
             type:CREATE_ORDER_FAIL,
+            payload:error.response.data.message,
+        })
+    }
+}
+
+// My Orders
+export const myOrders=()=>async(dispatch)=>{
+    try {
+        dispatch({
+            type:MY_ORDERS_REQUEST
+        });
+
+        const {data}=await axios.get("/api/v1/orders/me");
+        dispatch({type:MY_ORDERS_SUCCESS,payload:data.orders});
+    } catch (error) {
+        dispatch({
+            type:MY_ORDERS_FAIL,
+            payload:error.response.data.message,
+        })
+    }
+}
+// Get Order detials
+export const getOrderDetails=(id)=>async(dispatch)=>{
+    try {
+        dispatch({
+            type:ORDER_DETAILS_REQUEST
+        });
+
+        const {data}=await axios.get(`/api/v1/order/${id}`);
+        dispatch({type:ORDER_DETAILS_SUCCESS,payload:data.order});
+    } catch (error) {
+        dispatch({
+            type:ORDER_DETAILS_FAIL,
             payload:error.response.data.message,
         })
     }
