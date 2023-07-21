8fad0bb7aded11c392c2c41c09f3e2f8cd159f69
Athul <athulmaliyakkal@gmail.com>
2023-07-21 17:17:01 +0530

    Update
    Added new reducers to the store

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,10 +1,10 @@
 import {legacy_createStore as createStore,combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import { productDetailReducer, productReducer } from "./reducers/productReducer";
+import { newReviewReducer, productDetailReducer, productReducer } from "./reducers/productReducer";
 import { forgotPasswordReducer, profileReducer, userReducer } from "./reducers/userReducer";
 import { cartReducer } from "./reducers/cartReducer";
-import { newOrderReducer } from "./reducers/orderReducer";
+import { myOrdersReducer, newOrderReducer, orderDetailsReducer } from "./reducers/orderReducer";
 const reducer=combineReducers({
     products:productReducer,
     productDetails:productDetailReducer,
@@ -12,7 +12,10 @@ const reducer=combineReducers({
     profile:profileReducer,
     forgotPassword:forgotPasswordReducer,
     cart:cartReducer,
-    newOrder:newOrderReducer
+    newOrder:newOrderReducer,
+    myOrders:myOrdersReducer,
+    orderDetails:orderDetailsReducer,
+    newReview:newReviewReducer
 });
 
 let initialState = {
