4628a34736872c3df24fd41555e05dd61e1db16c
Athul <athulmaliyakkal@gmail.com>
2023-07-22 12:32:58 +0530

    Admin Product and New Product Actions

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -5,6 +5,13 @@ import { ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,CLEAR_ERRORS,
      NEW_REVIEW_REQUEST,
      NEW_REVIEW_SUCCESS,
      NEW_REVIEW_FAIL,
+     ADMIN_PRODUCT_REQUEST,
+     ADMIN_PRODUCT_SUCCESS,
+     ADMIN_PRODUCT_FAIL,
+    NEW_PRODUCT_REQUEST,
+    NEW_PRODUCT_SUCCESS,
+    NEW_PRODUCT_FAIL,
+    
      } from "../constants/productConstants";
 
 export const getProduct=(keyword="",currentPage=1,price=[0,25000 ],category,ratings=0)=>async(dispatch)=>{
@@ -28,6 +35,24 @@ export const getProduct=(keyword="",currentPage=1,price=[0,25000 ],category,rati
         });
     }
 };
+
+export const getAdminProduct=()=>async(dispatch)=>
+{
+    try{
+        dispatch({type:ADMIN_PRODUCT_REQUEST});
+        const {data}=await axios.get(`/api/v1/admin/products`);
+        dispatch({
+            type:ADMIN_PRODUCT_SUCCESS,
+            payload:data,
+        })
+    }catch(error)
+    {
+        dispatch({
+            type:ADMIN_PRODUCT_FAIL,
+            payload:error.response.data.message
+        });
+    }
+}
 export const getProductDetails=(id)=>async(dispatch)=>{
     try{
         dispatch({type:PRODUCT_DETAIL_REQUEST});
@@ -59,6 +84,25 @@ export const newReview=(reviewData)=>async(dispatch)=>{
     {
         dispatch({
             type:NEW_REVIEW_FAIL,
+            payload:error.response.data.message
+        });
+    }
+};
+export const newProduct=(productData)=>async(dispatch)=>{
+    try{
+        dispatch({type:NEW_PRODUCT_REQUEST});
+        const config={
+           headers: {"Content-Type":"application/json"}
+        }
+        const {data}=await axios.post(`/api/v1/admin/product/new`,productData,config);
+        dispatch({
+            type:NEW_PRODUCT_SUCCESS,
+            payload:data,
+        })
+    }catch(error)
+    {
+        dispatch({
+            type:NEW_PRODUCT_FAIL,
             payload:error.response.data.message
         });
     }
