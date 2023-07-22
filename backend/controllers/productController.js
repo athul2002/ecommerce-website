9d5c21b5cb34cdc44128ca34f5cb0edd38eb28ae
Athul <athulmaliyakkal@gmail.com>
2023-07-22 12:33:15 +0530

    Cloudinary Addition

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,9 +2,31 @@ const Product=require('../models/productModels');
 const ErrorHandler = require('../utils/errorHandler');
 const asyncError=require('../middleware/asyncError');
 const ApiFeatures = require('../utils/apiFeatures');
-
+const cloudinary = require("cloudinary");
 // Create Product ->Admin only
 exports.createProduct=asyncError(async(req,res,next)=>{
+  let images = [];
+
+  if (typeof req.body.images === "string") {
+    images.push(req.body.images);
+  } else {
+    images = req.body.images;
+  }
+
+  const imagesLinks = [];
+
+  for (let i = 0; i < images.length; i++) {
+    const result = await cloudinary.v2.uploader.upload(images[i], {
+      folder: "products",
+    });
+
+    imagesLinks.push({
+      public_id: result.public_id,
+      url: result.secure_url,
+    });
+  }
+
+  req.body.images = imagesLinks;
     req.body.user = req.user.id
     const product=await Product.create(req.body);
     res.status(201).json({
@@ -165,3 +187,13 @@ exports.deleteReviews=asyncError(async(req,res,next)=>{
         reviews:product.reviews,
     });
   }); 
+
+  // Get all product Admin
+exports.getAdminProducts=asyncError(async(req,res,next)=>{
+  const products= await Product.find();
+
+  res.status(200).json({success:true,
+      products,
+    });
+}
+)
