7beeabf01cfdf766c3c445e169801e9ab7766d21
Athul <athulmaliyakkal@gmail.com>
2023-07-24 16:54:48 +0530

    Contact Page

---
+++ frontend/src/component/layout/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Contact.css";
+import { Button } from "@mui/material";
+const Contact = () => {
+  return (
+    <div className="contactContainer">
+      <a className="mailBtn" href="mailto:athulkrishna080@gmail.com">
+        <Button>Contact: athulkrishna080@gmail.com</Button>
+      </a>
+    </div>
+  )
+}
+
+export default Contact
