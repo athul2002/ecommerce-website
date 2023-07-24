0c75aefadac289b6e7d2b5f73ef8dd151c4f9533
Athul <athulmaliyakkal@gmail.com>
2023-07-24 12:25:41 +0530

    Get all reviews and Delete Review

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 
-import { ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,CLEAR_ERRORS,
-     PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST,PRODUCT_DETAIL_SUCCESS,
-     NEW_REVIEW_REQUEST,
-     NEW_REVIEW_SUCCESS,
-     NEW_REVIEW_FAIL,
-     ADMIN_PRODUCT_REQUEST,
-     ADMIN_PRODUCT_SUCCESS,
-     ADMIN_PRODUCT_FAIL,
+import { ALL_PRODUCT_FAIL,
+    ALL_PRODUCT_REQUEST,
+    ALL_PRODUCT_SUCCESS,
+    CLEAR_ERRORS,
+    PRODUCT_DETAIL_FAIL, 
+    PRODUCT_DETAIL_REQUEST,
+    PRODUCT_DETAIL_SUCCESS,
+    NEW_REVIEW_REQUEST,
+    NEW_REVIEW_SUCCESS,
+    NEW_REVIEW_FAIL,
+    ADMIN_PRODUCT_REQUEST,
+    ADMIN_PRODUCT_SUCCESS,
+    ADMIN_PRODUCT_FAIL,
     NEW_PRODUCT_REQUEST,
     NEW_PRODUCT_SUCCESS,
     NEW_PRODUCT_FAIL,
@@ -17,6 +22,12 @@ import { ALL_PRODUCT_FAIL,ALL_PRODUCT_REQUEST,ALL_PRODUCT_SUCCESS,CLEAR_ERRORS,
     UPDATE_PRODUCT_REQUEST,
     UPDATE_PRODUCT_SUCCESS,
     UPDATE_PRODUCT_FAIL,
+    ALL_REVIEW_REQUEST,
+    ALL_REVIEW_SUCCESS,
+    ALL_REVIEW_FAIL,
+    DELETE_REVIEW_REQUEST,
+    DELETE_REVIEW_SUCCESS,
+    DELETE_REVIEW_FAIL,
      } from "../constants/productConstants";
 
 export const getProduct=(keyword="",currentPage=1,price=[0,25000 ],category,ratings=0)=>async(dispatch)=>{
@@ -146,6 +157,38 @@ export const updateProduct=(id,productData)=>async(dispatch)=>{
     {
         dispatch({
             type:UPDATE_PRODUCT_FAIL,
+            payload:error.response.data.message
+        });
+    }
+};
+export const getAllReviews=(id)=>async(dispatch)=>{
+    try{
+        dispatch({type:ALL_REVIEW_REQUEST});
+        const {data}=await axios.get(`/api/v1/reviews?id=${id}`);
+        dispatch({
+            type:ALL_REVIEW_SUCCESS,
+            payload:data.reviews,
+        });
+    }catch(error)
+    {
+        dispatch({
+            type:ALL_REVIEW_FAIL,
+            payload:error.response.data.message
+        });
+    }
+};
+export const deleteReviews=(id,productId)=>async(dispatch)=>{
+    try{
+        dispatch({type:DELETE_REVIEW_REQUEST});
+        const {data}=await axios.delete(`/api/v1/reviews?id=${id}&productId=${productId}`);
+        dispatch({
+            type:DELETE_REVIEW_SUCCESS,
+            payload:data.success,
+        });
+    }catch(error)
+    {
+        dispatch({
+            type:DELETE_REVIEW_FAIL,
             payload:error.response.data.message
         });
     }
